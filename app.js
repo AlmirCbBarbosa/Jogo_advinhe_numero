baa8b68ca902e4a252faddffc99642c014d48ea4
AlmirCbBarbosa <almirdelimaestudos@gmail.com>
2023-08-26 15:53:58 -0300

    Vericador de acerto e número de tentativas implementadas no app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let numeroSecreto;
 let valorMinimo = 0;
 let valorMaximo = 100;
 let palpiteUsuario;
+let numeroTentativa = 10;
 
 alert("Bem vindo ao jogo: Advinhe o Número Secreto");
 
@@ -11,9 +12,23 @@ alert("Bem vindo ao jogo: Advinhe o Número Secreto");
 
 numeroSecreto = numeroAleatorio(valorMinimo, valorMaximo); // cria um valor aleatório e amarzena na variavel
 
-console.log(`O número secreto é ${numeroSecreto}`);// pode apagar
+do{
 
-palpiteUsuario = prompt(`Tente adivinhar, informando um valor entre ${valorMinimo} e ${valorMaximo}`); // armazena o numero do usuário na variavel
+    palpiteUsuario = prompt(`Tente adivinhar, informando um valor entre ${valorMinimo} e ${valorMaximo}, você possui ${numeroTentativa}`); // armazena o numero do usuário na variavel
 
-console.log(`O palpite do usuario é ${palpiteUsuario}`)// pode apagar
+    if(palpiteUsuario == numeroSecreto){
+        alert(`Parabéns você acertou o número secreto!`);
+        
+    } else if( palpiteUsuario < numeroSecreto){
+        alert(`O número ${palpiteUsuario} é menor que o número secreto.`);
+        
+    } else if(palpiteUsuario > numeroSecreto){
+        alert(`O número ${palpiteUsuario} é maior que o número secreto.`);
+        
+    }
 
+    --numeroTentativa;
+
+} while( palpiteUsuario != numeroSecreto && numeroTentativa != 0);
+
+if(numeroTentativa == 0) alert(`Suas tentativas se esgotaram. O número secreto é ${numeroSecreto}!`);
