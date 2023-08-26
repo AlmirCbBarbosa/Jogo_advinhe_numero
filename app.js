68fe196defd47aa91ee351daa0f6646fe199c615
AlmirCbBarbosa <almirdelimaestudos@gmail.com>
2023-08-26 10:13:03 -0300

    Definindo parâmetros do número secreto e entradas no arquivo app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+let numeroSecreto;
+let valorMinimo = 0;
+let valorMaximo = 100;
+let palpiteUsuario;
+
+alert("Bem vindo ao jogo: Advinhe o Número Secreto");
+
+function numeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+numeroSecreto = numeroAleatorio(valorMinimo, valorMaximo); // cria um valor aleatório e amarzena na variavel
+
+console.log(`O número secreto é ${numeroSecreto}`);// pode apagar
+
+palpiteUsuario = prompt(`Tente adivinhar, informando um valor entre ${valorMinimo} e ${valorMaximo}`); // armazena o numero do usuário na variavel
+
+console.log(`O palpite do usuario é ${palpiteUsuario}`)// pode apagar
+
